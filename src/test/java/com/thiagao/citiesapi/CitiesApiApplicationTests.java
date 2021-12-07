ff8f30c69472628d6a104363b44637282d66cb32
thiagaoz <thiago.vazss@gmail.com>
2021-12-07 08:51:55 -0300

    Primeiro commit

---
+++ src/test/java/com/thiagao/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagao.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
